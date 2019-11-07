fdd55dc9b3c937996a372407e9b4d530f09aeff3
José Morales <jlmruiz@outlook.com>
2019-11-07 10:35:54 -0600

    updated until v.42

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -12,15 +12,24 @@ class JobsController extends BaseController {
 
     public function postAddJobAction($request) {
         $newJob = $request->getParsedBody();
+        $files =$request->getUploadedFiles();
         $responseMessage = null;
         if (!empty($newJob)) {
             $jobValidator = v::key('title', v::stringType()->notEmpty())
                 ->key('description', v::stringType()->notEmpty());
             try {
                 $jobValidator->assert($newJob);
+                $logo = $files['logo'];
+                $fileName = null;
+                if ($logo->getError() == UPLOAD_ERR_OK) {
+                    $fileName = $logo->getClientFilename();
+                    $fileName = "uploads/$fileName";
+                    $logo->moveTo($fileName);
+                }
                 $job = new Job();
                 $job->title = $newJob['title'];
                 $job->description = $newJob['description'];
+                $job->filename = $fileName;
                 $job->save();
                 $responseMessage = 'Saved!';
             } catch (\Exception $ex) {
